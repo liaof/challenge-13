77122265a997557bae154b08fb745a63f1b1ba16
liaof <f.liao163@gmail.com>
2021-05-22 22:35:28 -0400

    complete

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # E-commerce Back End Starter Code
 
+## [View Program in Action](https://youtu.be/yIQt0f0LfnE)
+
+## Summary
+
+This weeks challenge has us take some prewritten back end code, and initiate the objects, sync the server, finish up the routes, and create associations; all according to exacting specifications because this project simulates the finishing up of someone else's code. Instead of practicing creativity, this challenge is a study in adapability and following instructions.
+
+## Usage
+
+This program is meant to be run from the command line; you can then access the ports via Insomnia at this point. NOTE: this is *currently* ONLY THE BACKEND
+
 ## Testing
 
 *api/categories*
@@ -14,12 +24,24 @@
     "product_name": "banana",
     "price": 2.00,
     "stock": 14,
-    "category_id": 4,
-		"tagIds":3
+    "tagIds": [1, 2, 3]
+}
+
+put:
+
+{
+      "price": 2500,
+      "stock": 3,
+     "tagIds": [1, 2, 3, 4]
 }
 
 *api/tags*
 
 {
-    "tag_name": 1
+	"tag_name":"heybbbb",
+	"productIds":[1]
 }
+
+### Special Thanks
+Erik De Luna - Assistant Instructor
+Floyd Jones - Assistant Instructor
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -79,7 +79,7 @@ router.post('/', (req, res) => {
   // create a new tag
   Tag.create({
     tag_name: req.body.tag_name,
-    //productIds: req.body.productIds
+    productIds: req.body.productIds
   })
     .then((tag)=> {
      if(req.body.productIds.length) {
@@ -101,8 +101,46 @@ router.post('/', (req, res) => {
 });
 
 router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+  // update product data
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+    .then((tag) => {
+      // find all associated tags from ProductTag
+      return ProductTag.findAll({ where: { tag_id: req.params.id } });
+    })
+    .then((productTags) => {
+      // get list of current tag_ids
+      const productTagIds = productTags.map(({ product_id }) => product_id);
+      // create filtered list of new tag_ids
+      const newProductTags = req.body.productIds
+        .filter((product_id) => !productTagIds.includes(product_id))
+        .map((product_id) => {
+          return {
+            tag_id: req.params.id,
+            product_id
+          };
+        });
+      // figure out which ones to remove
+      const productTagsToRemove = productTags
+        .filter(({ product_id }) => !req.body.productIds.includes(product_id))
+        .map(({ id }) => id);
+
+      // run both actions
+      return Promise.all([
+        ProductTag.destroy({ where: { id: productTagsToRemove } }),
+        ProductTag.bulkCreate(newProductTags),
+      ]);
+    })
+    .then((updatedProductTags) => res.json(updatedProductTags))
+    .catch((err) => {
+      // console.log(err);
+      res.status(400).json(err);
+    });
 });
+
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
